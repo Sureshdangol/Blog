202a588ccfcbb92f7493e43beb3d61efca56798f
Sureshdangol <sureshdgl100@gmail.com>
2020-08-03 16:10:07 +0545

    password change working

--- members/urls.py
+++ members/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserRegisterView,UserEditView,PasswordsChangeView
+from django.contrib.auth import  views as auth_views
+from . import views
+
 
 urlpatterns = [
     #path('',views.home,name="home"),
     path('registration/',UserRegisterView.as_view(),name='register'),
+    path('edit_profile/',UserEditView.as_view(),name='edit_profile'),
+    path('password/',auth_views.PasswordChangeView.as_view(template_name='registration/change-password.html')),
+    #path('password/',PasswordsChangeView.as_view(template_name='registration/change-password.html')),
+    #path('password_sucess',views.password_sucess,name="password_sucess"),
+
 
 ]
